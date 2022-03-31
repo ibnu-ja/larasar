deffdafcfd810b185526a49b3aa4a68054a38e22
Ibnu <ibnu.magang@gmail.com>
2022-03-31 22:26:21 +0700

    feat(register): update to use quasar

--- resources/views/pages/Auth/Register.vue
+++ resources/views/pages/Auth/Register.vue
@@ -1,103 +1,166 @@
-<template>
-    <Head title="Register" />
+<script lang="ts" setup>
+import { Head, Link as InertiaLink, useForm, usePage } from '@inertiajs/inertia-vue3'
+import { computed, inject, ref } from 'vue'
+import { Jetstream } from '@/types/inertia-props'
+import WebLayout from '@/views/layouts/WebLayout.vue'
+import { useWidth } from '@/composables/useWidth'
+const route: any = inject('route')
+const showP = ref(false)
+const showCP = ref(false)
+const form = useForm({
+  name: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+  terms: false
+})
+const jetstream = computed(() => usePage().props.value.jetstream as Jetstream)
+function submit () {
+  form.post(route('register'), {
+    onFinish: () => form.reset('password', 'password_confirmation')
+  })
+}
 
-    <jet-authentication-card>
-        <template #logo>
-            <jet-authentication-card-logo />
-        </template>
-
-        <jet-validation-errors class="mb-4" />
-
-        <form @submit.prevent="submit">
-            <div>
-                <jet-label for="name" value="Name" />
-                <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
-            </div>
-
-            <div class="mt-4">
-                <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required />
-            </div>
-
-            <div class="mt-4">
-                <jet-label for="password" value="Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <jet-label for="password_confirmation" value="Confirm Password" />
-                <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4" v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature">
-                <jet-label for="terms">
-                    <div class="flex items-center">
-                        <jet-checkbox name="terms" id="terms" v-model:checked="form.terms" />
-
-                        <div class="ml-2">
-                            I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
-                        </div>
-                    </div>
-                </jet-label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Already registered?
-                </Link>
-
-                <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </jet-button>
-            </div>
-        </form>
-    </jet-authentication-card>
-</template>
-
-<script lang="ts">
-    import { defineComponent } from 'vue'
-    import JetAuthenticationCard from '@/views/jetstream/AuthenticationCard.vue'
-    import JetAuthenticationCardLogo from '@/views/jetstream/AuthenticationCardLogo.vue'
-    import JetButton from '@/views/jetstream/Button.vue'
-    import JetInput from '@/views/jetstream/Input.vue'
-    import JetCheckbox from '@/views/jetstream/Checkbox.vue'
-    import JetLabel from '@/views/jetstream/Label.vue'
-    import JetValidationErrors from '@/views/jetstream/ValidationErrors.vue'
-    import { Head, Link } from '@inertiajs/inertia-vue3';
-
-    export default defineComponent({
-        components: {
-            Head,
-            JetAuthenticationCard,
-            JetAuthenticationCardLogo,
-            JetButton,
-            JetInput,
-            JetCheckbox,
-            JetLabel,
-            JetValidationErrors,
-            Link,
-        },
-        
-        inject: ['route'],
-
-        data() {
-            return {
-                form: this.$inertia.form({
-                    name: '',
-                    email: '',
-                    password: '',
-                    password_confirmation: '',
-                    terms: false,
-                })
-            }
-        },
-
-        methods: {
-            submit() {
-                this.form.post(this.route('register'), {
-                    onFinish: () => this.form.reset('password', 'password_confirmation'),
-                })
-            }
-        }
-    })
+const { width } = useWidth()
 </script>
+
+<template>
+  <Head title="Register" />
+
+  <web-layout>
+    <q-page
+      :padding="$q.screen.gt.sm"
+      class="flex flex-center"
+    >
+      <q-card
+        :bordered="$q.screen.lt.sm"
+        :flat="$q.screen.lt.sm"
+        :square="$q.screen.lt.sm"
+        class="q-mt-xl"
+        :style="'width: ' + width"
+      >
+        <q-form @submit.prevent="submit">
+          <q-card-section>
+            <inertia-link
+              href="/"
+              class="nostyle cursor-pointer"
+            >
+              <q-avatar
+                size="100px"
+                color="white"
+                class="absolute-center shadow-10"
+              >
+                <svg
+                  viewBox="0 0 48 48"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z"
+                    fill="#6875F5"
+                  />
+                  <path
+                    d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z"
+                    fill="#6875F5"
+                  />
+                </svg>
+              </q-avatar>
+            </inertia-link>
+          </q-card-section>
+          <q-card-section>
+            <div class="text-center q-pt-lg">
+              <div class="col text-h6 ellipsis">
+                Register
+              </div>
+            </div>
+          </q-card-section>
+          <q-card-section class="q-gutter-md">
+            <q-input
+              v-model="form.email"
+              :error="!!form.errors.email"
+              :error-message="form.errors.email"
+              outlined
+              label="Email"
+              lazy-rules
+            />
+
+            <q-input
+              v-model="form.password"
+              :error="!!form.errors.password"
+              :error-message="form.errors.password"
+              :type="showP? 'password' : 'text'"
+              outlined
+              label="Password"
+              lazy-rules
+            >
+              <template #append>
+                <q-icon
+                  :name="showP ? 'mdi-eye' : 'mdi-eye-off'"
+                  class="cursor-pointer"
+                  @click="showP = !showP"
+                />
+              </template>
+            </q-input>
+
+            <q-input
+              v-model="form.password_confirmation"
+              :error="!!form.errors.password_confirmation"
+              :error-message="form.errors.password_confirmation"
+              :type="showCP? 'password' : 'text'"
+              outlined
+              label="Password confirmation"
+              lazy-rules
+            >
+              <template #append>
+                <q-icon
+                  :name="showCP ? 'mdi-eye' : 'mdi-eye-off'"
+                  class="cursor-pointer"
+                  @click="showCP = !showCP"
+                />
+              </template>
+            </q-input>
+
+            <q-field
+              v-if="jetstream.hasTermsAndPrivacyPolicyFeature"
+              borderless
+              :error="!!form.errors.terms"
+              :error-message="form.errors.terms"
+              dense
+            >
+              <template #control>
+                <q-checkbox
+                  v-model="form.terms"
+                  :color="!!form.errors.terms ? 'negative' : 'primary'"
+                >
+                  <span :class="{'text-negative': !!form.errors.terms }">
+                    I agree to the <a
+                      href="/terms"
+                      target="_blank"
+                      @click.stop
+                    >terms of service</a>
+                  </span>
+                </q-checkbox>
+              </template>
+            </q-field>
+          </q-card-section>
+          <q-separator />
+          <q-card-actions align="right">
+            <inertia-link
+              :href="route('login')"
+              class="q-mx-md"
+            >
+              Already registered?
+            </inertia-link>
+            <q-btn
+              color="primary"
+              flat
+              type="submit"
+            >
+              Login
+            </q-btn>
+          </q-card-actions>
+        </q-form>
+      </q-card>
+    </q-page>
+  </web-layout>
+</template>
