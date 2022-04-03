4486d07fe7bfa8f47364b0aca44e1cd2464fbf5f
Ibnu <ibnu.magang@gmail.com>
2022-04-03 22:35:41 +0700

    feat: create ActionSection component

---
+++ resources/views/components/ActionSection.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { computed, useSlots } from 'vue'
+
+const slots = useSlots()
+
+const hasActions = computed(() => !!slots.actions)
+const hasStatus = computed(() => !!slots.status)
+
+</script>
+
+<template>
+  <div class="row q-col-gutter-md">
+    <div class="col-12 col-md-4">
+      <h5>
+        <slot name="title" />
+      </h5>
+
+      <p><slot name="description" /></p>
+    </div>
+    <div class="col-12 col-md-8">
+      <div>
+        <q-card class="q-pt-md">
+          <q-card-section class="q-gutter-md q-pt-none">
+            <h6 v-if="hasStatus">
+              <slot name="status" />
+            </h6>
+
+            <slot name="content" />
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-actions
+            v-if="hasActions"
+          >
+            <slot name="actions" />
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+  </div>
+</template>
